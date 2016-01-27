1ba2a51c5cf944049d1b83abe8df2d81458b8385
Sean Trott <seantrott@icsi.berkeley.edu>
2016-01-27 14:29:57 -0800

    utils

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ python3 src/main/robots/ros_solver.py ProblemSolver &
 
 #export JYTHONPATH=../framework_code/build/compling.core.jar:../framework_code/src/main/nluas/language
 #jython -m analyzer ../ecg_grammars/compRobots.prefs &
-python3 src/main/robots/robots_ui.py AgentUI 
+#python3 src/main/robots/robots_ui.py AgentUI 
 
