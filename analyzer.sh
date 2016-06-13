e784835cf58c3d03b6cf74752c2335fcb8935b81
Sean Trott <trott.sean@gmail.com>
2016-06-13 11:09:55 -0700

    Update analyzer.sh

--- analyzer.sh
+++ analyzer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #sh starter.sh &
 export ECG_FED=FED1
-export JYTHONPATH=../framework_code/build/compling.core.jar:../framework_code/src/main/nluas/language
+export JYTHONPATH=../ecg_framework_code/build/compling.core.jar:../ecg_framework_code/src/main/nluas/language
 #jython -J-Xmx4g -m analyzer ../ecg_grammars/compRobots.prefs 
 jython -m analyzer ../ecg_grammars/compRobots.prefs 
 #python3 src/main/robots/robots_ui.py AgentUI 
